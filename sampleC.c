6c7680c71be7ada61a67edd536189a6b43bcae89
Ravi <Ravi98kapse@gmail.com>
2021-10-07 19:00:32 +0530

    This file is the first copy

--- sampleC.c
+++ sampleC.c
@@ -3,9 +3,9 @@ void addtwo()
 {    int num1,num2;
 	printf("Enter two numbers" );
 	scanf("%d %d ",&num1,&num2);
-	printf("The sum of %d and %d number  is %d",num1,num2,num1+num2);
+	printf("The sum of two number  is %d",num1+num2);
 }
 void main()
 {
-   addtwo();
+   
 }
